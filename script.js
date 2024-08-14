7b5024d445f828d43556060087cf3a1107123c08
solard12 <ilyasmoubajjal23@gmail.com>
2024-08-14 14:49:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-window.addEventListener('scroll', function() {
-    const fadeIns = document.querySelectorAll('.fade-in');
-    const windowHeight = window.innerHeight;
 
-    fadeIns.forEach(function(el) {
-        const elementTop = el.getBoundingClientRect().top;
-        if (elementTop < windowHeight - 100) {
-            el.classList.add('show');
-        }
-    });
-});
